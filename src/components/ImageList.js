71294153eca106d5c4cac80efacecbdf58033e3d
Andres <andresdomlez@gmail.com>
2021-10-09 00:33:04 +0100

    images lists and cards components and styles

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,15 +1,11 @@
-import './ImageList.css';
-import React from 'react';
+import "./ImageList.css";
+import React from "react";
+import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
-  const images = props.images.map(({ description, id, urls }) => {
-    return (
-      <div key={id}>
-        <img src={urls.regular} alt={description} />;
-      </div>
-    );
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />;
   });
-
   return <div className="image-list">{images}</div>;
 };
 
