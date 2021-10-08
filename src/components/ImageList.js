8768aa3c85f571caeaf00c311ecd91eacdafb9cc
Andres <andresdomlez@gmail.com>
2021-10-08 13:56:23 +0100

    unsplash render images

---
+++ src/components/ImageList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ImageList = (props) => {
+  const images = props.images.map(({ description, id, urls }) => {
+    return (
+      <div key={id}>
+        <img src={urls.regular} alt={description} />;
+      </div>
+    );
+  });
+
+  return <div>{images}</div>;
+};
+
+export default ImageList;
